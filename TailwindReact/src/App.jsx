a406bac103e46f236395c8ee32d7fce0f6817cd9
Avinash Sharma <Avinashsharma31384@gmail.com>
2024-05-10 09:44:28 +0530

    Hello

---
+++ TailwindReact/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from './component/card'
+function App() {
+  return (
+    <>
+      <h1 className='bg-white text-black rounded-xl p-10'>Avinash Sharma</h1>
+      <Card/>
+      <Card/>
+      <Card/>
+    </>
+  )
+}
+
+export default App
